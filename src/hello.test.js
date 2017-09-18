7a6d264bf4f2e01cfee29beb48e87ef32249fa3a
Sendhil Kumar R <sendhil.r@gmail.com>
2017-09-18 19:49:45 +0530

    Added jest tests using enzyme

--- src/hello.test.js
+++ src/hello.test.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import App from './App.jsx';
+import store from './store';
+
 test('adds 1 + 2 to equal 3', () => {
   expect(1+2).toBe(3);
 }); 
+test('allows us to set props', () => {
+  const wrapper = mount((<Provider store={store}>
+    <App />
+  </Provider>));
+  expect(wrapper).toBeTruthy();
+});
