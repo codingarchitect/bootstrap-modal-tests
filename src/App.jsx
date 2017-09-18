9a9ef26ee09310903248b7289b7a4f80b3ca8efa
Sendhil Kumar R <sendhil.r@gmail.com>
2017-09-18 19:41:50 +0530

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Alert from 'react-bootstrap/lib/Alert';
 
 const App = ({ dispatch }) => (
   <div>
     <h1>Hello from react!</h1>
     <button onClick={() => dispatch({ type: 'PING' })}>Dispatch Ping</button>
     <div>Check the console to see if ping was mapped to pong by cycle on Dispatch Ping</div>
+    <Alert bsStyle="info">
+      Hello from React Bootstrap
+    </Alert>
   </div>
 );
 
