79095526cb2280bc95a9ae46ec655cedff4cd23b
Sendhil Kumar R <sendhil.r@gmail.com>
2017-10-24 12:48:17 +0530

    Removed the settimeout call which seems to be redundant

--- src/hello.test.js
+++ src/hello.test.js
@@ -9,14 +9,11 @@ import store from './store';
 test('adds 1 + 2 to equal 3', () => {
   expect(1+2).toBe(3);
 }); 
-test('allows us to find close-button', (done) => {
+test('allows us to find close-button', () => {
   const wrapper = mount((<Provider store={store}>
     <App />
   </Provider>));
   wrapper.find('.show-dialog').simulate('click');
-  setTimeout(() => {
-    const closeButton = wrapper.find('.close-button');
-    closeButton.last().simulate('click');
-    done();
-  }, 3000);
+  const closeButton = wrapper.find('.close-button');
+  closeButton.last().simulate('click');  
 });
